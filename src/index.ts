f0f621797fd001312c2158127766df6b456af411
mpilopillz <mpilo.dlamini@yahoo.com>
2021-04-02 20:51:17 +0200

    preparing for generic user collection

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
-import { User } from "./models/User";
+import { Collection } from "./models/Collection";
 
-const user = User.buildUser({ id: 1 });
-user.on("change", () => {
-  console.log(user);
+const collection = new Collection("http://localhost:3004/users");
+collection.on("change", () => {
+  console.log(collection);
 });
+collection.fetch();
 
-user.fetch();
+// import axios, { AxiosResponse } from "axios";
+
+// axios.get("http://localhost:3004/users").then((response) => {
+//   console.log(response.data);
+// });
+// import { User } from "./models/User";
+
+// const user = User.buildUser({ id: 1 });
+// user.on("change", () => {
+//   console.log(user);
+// });
+
+// user.fetch();
